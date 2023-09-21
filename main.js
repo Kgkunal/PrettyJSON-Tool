e3e401389f0eb9d40698dc563a6f0daf006a95a3
Kunal D Gavhane <kgkunalgavhane1721@gmail.com>
2023-09-21 18:18:35 +0530

    Copy result button added

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ const inputArea = document.querySelector(".large-area--input");
 const outputArea = document.querySelector(".large-area--output");
 const btnFormat = document.querySelector(".controls--button--format");
 const btnMinify = document.querySelector(".controls--button--minify");
+const btnCopy = document.querySelector(".controls--button--copyresult");
 
 btnFormat.addEventListener("click", ()=>{
     const formatted = JSON.stringify(JSON.parse(inputArea.value) , null , 4);
@@ -12,3 +13,8 @@ btnMinify.addEventListener("click", ()=>{
     const minified = JSON.stringify(JSON.parse(inputArea.value) );
     outputArea.value = minified;
 });
+
+btnCopy.addEventListener("click",()=>{
+    navigator.clipboard.writeText(`${outputArea.value}`);
+})
+
